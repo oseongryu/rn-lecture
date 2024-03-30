df96bbfc817a75aa66de8150842313da82c90200
oseongryu <oseongryu@gmail.com>
2024-03-30 14:16:09 +0900

    ch3.1. core components

--- App.js
+++ App.js
@@ -1,11 +1,23 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { TextInput, Image,StyleSheet, Text, View, ScrollView, Button, Switch } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Button
+        title='Click me!'
+        onPress={()=>{
+            console.log('Button Clicked!')
+        }}
+         />
+         <Switch value={true} />
+         <Switch value={false} />
+      <Text style={styles.text}>Open up App.js to start working on your app!</Text>
+      <ScrollView>
+        <Image source={require("./cute-puppy.jpeg")} style={styles.local_image} />
+        <Image source={{uri: "https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcQgByBT5IiAT_a2x9pUVb4VMoOrlzHH7Jrzj-HB5jzHlR4lNLMS"}} style={styles.url_image} />
+        <TextInput placeholder='내용을 입력해주세요' />
+      </ScrollView>
     </View>
   );
 }
@@ -17,4 +29,17 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  text: {
+    fontSize: 20,
+    fontWeight: "bold",
+  },
+  local_image: {
+    width:100,
+    height:100
+  },
+  url_image: {
+    width:200,
+    height:200
+  }
+
 });
