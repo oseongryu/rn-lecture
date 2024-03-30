49371d67ce2bd6cd4ade29c9b60130e03e5feb54
oseongryu <oseongryu@gmail.com>
2024-03-30 15:00:53 +0900

    ch3.3.

--- App.js
+++ App.js
@@ -1,12 +1,25 @@
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, View, Text, Image } from "react-native";
+
+/**
+ * Header
+ * MyProfile
+ * Division
+ * FriendSection
+ * FriendList
+ */
 
 const Header = (props) => {
-  console.log("props of Header", props);
   return <Text>{props.title}</Text>;
 };
 const MyProfile = () => {
-  return <Text>MyProfile</Text>;
+  return (
+    <Profile
+      name="지은"
+      uri="https://shop.peopet.co.kr/data/goods/31/2021/11/4953_tmp_0f4c02bad88abd5ec334e3dff1cc92bc2245view.jpg"
+      profileSize={40}
+    />
+  );
 };
 const Division = () => {
   return <Text>Division</Text>;
@@ -14,34 +27,77 @@ const Division = () => {
 const FriendSection = () => {
   return <Text>FriendSection</Text>;
 };
-
-/**
- * @description Functional Component
- */
-const Profile = (props) => {
-  return <Text>{props.name}</Text>;
-};
-/**
- * @description Class Component
- */
-// class Profile extends React.Component {
-//   render() {
-//     return <Text>{this.props.name}</Text>;
-//   }
-const FreindList = () => {
+const FriendList = () => {
   return (
     <View>
-      <Profile name="민호" />
-      <Profile name="지연" />
-      <Profile name="예지" />
-      <Profile name="지연" />
-      <Profile name="세인" />
-      <Profile name="유림" />
-      <Profile name="지현" />
-      <Profile name="현서" />
+      <Profile
+        name="지연"
+        uri="https://s3.ap-northeast-2.amazonaws.com/elasticbeanstalk-ap-northeast-2-176213403491/media/magazine_img/magazine_262/%EC%8D%B8%EB%84%A4%EC%9D%BC.jpg"
+        profileSize={30}
+      />
+      <Profile
+        name="세인"
+        uri="https://shop.peopet.co.kr/data/goods/31/2021/11/4953_tmp_0f4c02bad88abd5ec334e3dff1cc92bc2245view.jpg"
+        profileSize={30}
+      />
+      <Profile
+        name="지현"
+        uri="https://shop.peopet.co.kr/data/goods/31/2021/11/4953_tmp_0f4c02bad88abd5ec334e3dff1cc92bc2245view.jpg"
+        profileSize={30}
+      />
+      <Profile
+        name="현서"
+        uri="https://shop.peopet.co.kr/data/goods/31/2021/11/4953_tmp_0f4c02bad88abd5ec334e3dff1cc92bc2245view.jpg"
+        profileSize={30}
+      />
+      <Profile
+        name="유림"
+        uri="https://shop.peopet.co.kr/data/goods/31/2021/11/4953_tmp_0f4c02bad88abd5ec334e3dff1cc92bc2245view.jpg"
+        profileSize={30}
+      />
     </View>
   );
 };
+/**
+ * Functional Component
+ */
+const Profile = (props) => {
+  return (
+    <View style={{ flexDirection: "row" }}>
+      <Image
+        source={{
+          uri: props.uri,
+        }}
+        style={{
+          width: props.profileSize,
+          height: props.profileSize,
+        }}
+      />
+      <Text>{props.name}</Text>
+    </View>
+  );
+};
+/**
+ * Class Component
+ */
+// class Profile extends React.Component {
+//   render() {
+//     return (
+//       <View style={{ flexDirection: "row" }}>
+//         <Image
+//           source={{
+//             uri: this.props.uri,
+//           }}
+//           style={{
+//             width: this.props.profileSize,
+//             height: this.props.profileSize,
+//           }}
+//         />
+//         <Text>{this.props.name}</Text>
+//       </View>
+//     );
+//   }
+// }
 
 export default function App() {
   return (
@@ -50,7 +106,7 @@ export default function App() {
       <MyProfile />
       <Division />
       <FriendSection />
-      <FreindList />
+      <FriendList />
     </View>
   );
 }
