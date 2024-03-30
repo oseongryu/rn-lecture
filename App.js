90d9a35c32dd33b0b6ba8eed99dfb51753fb93c6
oseongryu <oseongryu@gmail.com>
2024-03-30 16:52:23 +0900

    calculator

--- App.js
+++ App.js
@@ -1,12 +1,12 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+
+import { SafeAreaView, StyleSheet } from 'react-native';
+import Calculator from './src/Calculator';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <Calculator />
+    </SafeAreaView>
   );
 }
 
